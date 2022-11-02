2d53c4e38dead328f12e66b3e5091d24fda90688
De Chun Yuan <yuandechunsh@163.com>
2022-11-02 15:46:23 +0800

    update

--- app-gateway/src/main/java/com/app/config/GatewayLogFilter.java
+++ app-gateway/src/main/java/com/app/config/GatewayLogFilter.java
@@ -9,9 +9,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 /**
  * @author dc.yuan
  * @version 1.0
@@ -24,20 +21,18 @@ public class GatewayLogFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        String beginTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
+        Long beginTime = System.currentTimeMillis();
         exchange.getAttributes().put("beginTime", beginTime);
         return chain.filter(exchange).then(
                 Mono.fromRunnable(() -> {
                     Long startTime = Long.valueOf(exchange.getAttribute("beginTime"));
-                    if (startTime != null) {
-                        log.info("-----------Gateway打印日志开始-------------");
-                        log.info("访问接口host: " + exchange.getRequest().getURI().getHost());
-                        log.info("访问接口端口: " + exchange.getRequest().getURI().getPort());
-                        log.info("访问接口URL: " + exchange.getRequest().getURI().getPath());
-                        log.info("访问参数: " + exchange.getRequest().getQueryParams());
-                        log.info("访问时间: " + (Long.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"))) - startTime) + "ms");
-                        log.info("-----------Gateway打印日志结束-------------");
-                    }
+                    log.info("-----------Gateway打印日志开始-------------");
+                    log.info("访问接口host: " + exchange.getRequest().getURI().getHost());
+                    log.info("访问接口端口: " + exchange.getRequest().getURI().getPort());
+                    log.info("访问接口URL: " + exchange.getRequest().getURI().getPath());
+                    log.info("访问参数: " + exchange.getRequest().getQueryParams());
+                    log.info("访问时间: " + (System.currentTimeMillis() - startTime) + "ms");
+                    log.info("-----------Gateway打印日志结束-------------");
                 })
         );
     }
